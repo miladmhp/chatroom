03f2daa469175e15d30e4301d7980d4c6c900636
miladhp <hosseinpour@bugloos.nl>
2018-05-06 18:47:29 +0430

    group chat users card implemented and responsive bugs fixed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     "use strict";
+    var persian = {0:'۰',1:'۱',2:'۲',3:'۳',4:'۴',5:'۵',6:'۶',7:'۷',8:'۸',9:'۹'};
+    function traverse(el){
+          if(el.nodeType==3){
+              var list=el.data.match(/[0-9]/g);
+              if(list!=null && list.length!=0){
+                  for(var i=0;i<list.length;i++)
+                      el.data=el.data.replace(list[i],persian[list[i]]);
+              }
+          }
+          for(var i=0;i<el.childNodes.length;i++){
+              traverse(el.childNodes[i]);
+          }
+      }
+
+    traverse(document.body);
 
     $(".user .account").on("click", function(){
         $(".container-full .video-chat").css("margin-left", "0px");
